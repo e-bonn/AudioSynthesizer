92bc2d111616efaa752152f3d510eee18418bdc1
Eric Bonn <eribon41@gmail.com>
2017-03-06 02:22:51 -0500

    got simple sounds working

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
-(function(){
+(function()
+{
 
-var init = function(){
+var mySynth = null;
+
+var init = function()
+{
+	mySynth = new Synth();
 };
+
+document.addEventListener("DOMContentLoaded", function(event)
+{
+    init();
+});
+
+document.addEventListener("keydown", function(event)
+{
+	mySynth.playSound(233);
+});
+
+document.addEventListener("keyup", function(event)
+{
+	mySynth.stopSound(233);
+});
 
 })();
---
+++ js/wave.js
@@ -0,0 +1,46 @@
+function Wave(type)
+{
+    this.init(type);
+}
+
+Wave.prototype.init = function(type)
+{
+	this.real = [];
+    this.im = [];
+
+    this.buildWaveForm(type);
+}
+
+Wave.prototype.buildWaveForm = function(type)
+{
+    this.real = new Float32Array(4096);
+    this.im = new Float32Array(4096);
+
+    console.log(type);
+
+    if(type == "sine")
+    {
+        this.im[1] = 1.0;
+    }
+    else if (type == "square")
+    {
+        for(var k = 1; k < this.im.length; k += 2)
+        {
+            this.im[k] = 4.0/(Math.PI*k);
+        }
+    }
+    else if (type == "triangle")
+    {
+        for(var k = 1; k < this.im.length; k += 2)
+        {
+            this.im[k] = (8.0*Math.pow(-1,k+1))/(Math.pow(Math.PI,2)*Math.pow(k,2));
+        }
+    }
+    else if (type == "sawtooth")
+    {
+        for(var k = 1; k < this.im.length; k++)
+        {
+            this.im[k] = (2.0*Math.pow(-1,k))/(Math.PI*k);
+        }
+    }
+}
