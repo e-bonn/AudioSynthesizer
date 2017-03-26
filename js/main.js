1d4b814223c01867a645a025bd12fcc17fc99c2a
patrickpotok <patrick_potok@hotmail.com>
2017-03-26 19:49:20 -0400

    Finished UI for oscillator controls

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ var mySynth = null;
 
 var init = function() {
   mySynth = new Synth();
+  initKnobs();
 };
 
 document.addEventListener("DOMContentLoaded", function(event) {
@@ -32,6 +33,39 @@ var draw = function() {
   for (var analyser in mySynth.analysers) {
 
   }
+}
+
+var initKnobs = function() {
+  $(".knobs").knob({
+    'angleArc'    : 180,
+    'angleOffset' : 270,
+    'bgColor'     : '#AAAAAA',
+    'fgColor'     : '#33FFFF',
+    'height'      : 80,
+    'inputColor'  : '#33FFFF',
+    'max'         : 100,
+    'min'         : 0,
+    'step'        : 1,
+    'width'       : 80
+  });
+  $(".knobs").prop('readonly', true);
+
+  $(".freq-knobs").trigger(
+    'configure',
+    {
+      'max' : 25,
+      'min' : 1
+    }
+  );
+
+  $(".time-knobs").trigger(
+    'configure',
+    {
+      'max'  : 2,
+      'min'  : 0,
+      'step' : 0.1,
+    }
+  );
 }
 
 })();
